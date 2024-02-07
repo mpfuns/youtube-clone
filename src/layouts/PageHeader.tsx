475d162741854ef2aab5b7f53ffd873fa34630ee
Monet <monetpaul23@gmail.com>
2024-02-07 18:02:31 -0500

    first commit amd  working on pageheader

---
+++ src/layouts/PageHeader.tsx
@@ -0,0 +1,9 @@
+export default function PageHeader() {
+  return (
+    <div className=" flex gap-10  lg:gap-20 justify-between">
+        <div className=" flex "></div>
+        <div className=""></div>
+        <div className=""></div>
+    </div>
+  )
+}
