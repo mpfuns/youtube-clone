18e5107bad2b58cc6dbead6b8bd40a982bddebba
Monet <monetpaul23@gmail.com>
2024-02-13 20:24:30 -0500

    working on  video  playing in  the  thumbnail

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -1,3 +1,5 @@
+import formatDuration from "../utils/formatDuration"
+import { formatTimeAgo } from "../utils/formatTimeAgo"
 
  type VideoGridItemProps = {
     id: string
@@ -14,6 +16,8 @@
     videoUrl: string
  }
 
+ const VIEW_FORMATTER = new Intl.NumberFormat( undefined, { notation: 'compact' } )
+
 export default function VideoGridItem({
     id, 
     title, 
@@ -26,11 +30,27 @@ export default function VideoGridItem({
 }: VideoGridItemProps) {
   return (
     <div className=" flex flex-col gap-2 ">
-        <a href={`/watch?v=${id}`} className="relative  aspect-video">
+        <a href={`/watch?v=${id}`} className="relative aspect-video">
             <img src={thumbnailUrl} className="block w-full h-full object-cover rounded-xl" />
+            <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
+            { formatDuration(duration)}
+        </div>
         </a>
-        <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-.5 rounded">
-            {duration}
+        <div className=" flex gap-2">
+            <a href={`/@${channel.id}`} className="flex-shrink-0" >
+                <img src={channel.profileUrl}  className="w-12 h-12 rounded-full" />
+            </a>
+            <div className=" flex flex-col">
+                <a href={`/watch?v=${id}`} className="font-bold">
+                    {title}
+                </a>
+                <a href={`/@${channel.id}`} className="text-secondary-text text-sm" >
+                    {channel.name}
+                </a>
+                <div className="text-secondary-text text-sm">
+                {VIEW_FORMATTER.format(views)} Views ● {formatTimeAgo(postedAt)}
+                </div>
+            </div>
         </div>
     </div>
   )
