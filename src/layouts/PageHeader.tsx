01a3ef0b0c92f03b71581069435969f646a051b9
Monet <monetpaul23@gmail.com>
2024-02-25 14:39:15 -0500

    done  with  sidebar

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -2,23 +2,24 @@ import { ArrowLeft, Bell, Menu, Mic, Search, Upload, User } from "lucide-react"
 import logo from '../assets/logo.png';
 import Button from "../components/Button";
 import { useState } from "react";
+import { useSidebarContext } from "../contexts/SidebarContext";
 export default function PageHeader() {
    const [showFullWidth, setShowFullWidth] = useState(false);
+ 
   return (
     <div className=" flex gap-10  lg:gap-20 justify-between pt-2 mb-6 mx-4">
-        <div className={`gap-4 items-center flex-shrink-0 ${ showFullWidth? "hidden" : "flex"}`}>
-          <Button variant='ghost' size= 'icon' >
-            <Menu />
-          </Button>
-          <a href="/"> <img src={logo} alt=""  className="h-6"/></a>
-        </div>
+        
+        <PageHeaderFirstSection  hidden={showFullWidth} />
+        
+        <form className={`gap-4 flex-grow justify-center ${ showFullWidth? "flex" : "hidden md:flex "}`}>
+         
+       
         { showFullWidth && (
           <Button onClick={()=> setShowFullWidth(false)} size="icon"  variant="ghost" type="button"  className="flex-shrink-0">
           <ArrowLeft />
         </Button>
         )}
-        
-        <form className={`gap-4 flex-grow justify-center ${ showFullWidth? "flex" : "hidden md:flex "}`}>
+         
           <div className="flex flex-grow  max-w-[600px]">
           <input type="search"  placeholder="Search"  className=" rounded-l-full border  border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full  focus:border-blue-500 outline-none
           " />
@@ -50,3 +51,19 @@ export default function PageHeader() {
     </div>
   )
 }
+
+type  PageHeaderFirstSectionProps ={
+  hidden?:boolean
+}
+
+  export function PageHeaderFirstSection({ hidden = false } : PageHeaderFirstSectionProps){
+  const  {toggle} =useSidebarContext()
+  return(
+    <div className={`gap-4 items-center flex-shrink-0 ${ hidden? "hidden" : "flex"}`}>
+          <Button onClick={toggle} variant='ghost' size= 'icon' >
+            <Menu />
+          </Button>
+          <a href="/"> <img src={logo} alt=""  className="h-6"/></a>
+        </div>
+   )
+ }
--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,39 +1,99 @@
-import { ChevronDown, ChevronUp, Clapperboard, Clock, History, Home, Library, ListVideo, PlaySquare, Repeat } from 'lucide-react'
+import { ChevronDown, ChevronUp, Clapperboard, Clock, Film, Flame, Gamepad2, History, Home, Library, Lightbulb, ListVideo, Music2, Newspaper, PlaySquare, Podcast, Radio, Repeat, Shirt, ShoppingBag, Trophy } from 'lucide-react'
 import React, { Children, ElementType, ReactNode, useState } from 'react'
 import Button, { buttonStyles } from '../components/Button';
 import { twMerge } from 'tailwind-merge';
-import { playlists } from '../data/sidebar';
+import { playlists, subscriptions } from '../data/sidebar';
+import { useSidebarContext } from '../contexts/SidebarContext';
+import { PageHeaderFirstSection } from './PageHeader';
 
 export default function Sidebar() {
+     const { isLargeOpen, isSmallOpen, close} = useSidebarContext()
+
   return (
     <>
-    <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+    <aside className= {`sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 ${isLargeOpen? "lg:hidden" : "lg:flex" }`}>
         <SmallSidebarItems Icon={Home} title="Home" url="/" />
         <SmallSidebarItems Icon={Repeat} title="Shorts" url="/shorts" />
         <SmallSidebarItems Icon={Clapperboard} title="Subscriptions" url="/subscriptions" />
         <SmallSidebarItems Icon={Library} title="Library" url="/library" />
     </aside>
-    <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex">
+    { isSmallOpen && (
+    <div onClick={close} className=' lg:hidden fixed inset-0 z-[999] bg-secondary-dark opacity-50'/>
+)}
+    <aside className= {`w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2  ${isLargeOpen? "lg:flex" : "lg:hidden" } ${isSmallOpen ? "flex z-[999] bg-white max-h-screen":"hidden" } `}>
+<div className='lg:hidden pt-2 pb-4 px-2 sticky top-0  bg-white'>
+<PageHeaderFirstSection />
+</div>
+
 <LargeSidebarSection>
-    <LargeSidebarItem isActive Icon={Home} title="Home" url="/" />
-    <LargeSidebarItem  Icon={Clapperboard} title="Subscriptions" url="/subscriptions" />
+    <LargeSidebarItem isActive IconOrImgUrl={Home} title="Home" url="/" />
+    <LargeSidebarItem  IconOrImgUrl={Clapperboard} title="Subscriptions" url="/subscriptions" />
 </LargeSidebarSection>
     <hr />
     <LargeSidebarSection visibleItemCount={5}>
-    <LargeSidebarItem  Icon={Library} title="Library" url="/library" />
-    <LargeSidebarItem  Icon={History} title="History" url="/history" />
-    <LargeSidebarItem  Icon={PlaySquare} title="Your Videos" url="/your-videos" />
-    <LargeSidebarItem  Icon={Clock} title="Watch Later" url="/playlist?list=WL" />
-    {playlists.map(playlist => ( <LargeSidebarItem key={playlist.id}  Icon={ListVideo} title={playlist.name} url={`/playlist?list=${playlist.id}`} />))}
+    <LargeSidebarItem  IconOrImgUrl={Library} title="Library" url="/library" />
+    <LargeSidebarItem  IconOrImgUrl={History} title="History" url="/history" />
+    <LargeSidebarItem  IconOrImgUrl={PlaySquare} title="Your Videos" url="/your-videos" />
+    <LargeSidebarItem  IconOrImgUrl={Clock} title="Watch Later" url="/playlist?list=WL" />
+    {playlists.map(playlist => ( <LargeSidebarItem key={playlist.id}  IconOrImgUrl={ListVideo} title={playlist.name} url={`/playlist?list=${playlist.id}`} />))}
     </LargeSidebarSection>
     <hr />
+   <LargeSidebarSection  title="Subscription">
+    {subscriptions.map(subscription => (  <LargeSidebarItem key={subscription.id} IconOrImgUrl={subscription.imgUrl} title={subscription.channelName} url={`/@${subscription.id}`}/>))}
+   </LargeSidebarSection>
+    <hr />
+    <LargeSidebarSection title="Explore">
+          <LargeSidebarItem
+            IconOrImgUrl={Flame}
+            title="Trending"
+            url="/trending"
+          />
+          <LargeSidebarItem
+            IconOrImgUrl={ShoppingBag}
+            title="Shopping"
+            url="/shopping"
+          />
+          <LargeSidebarItem IconOrImgUrl={Music2} title="Music" url="/music" />
+          <LargeSidebarItem
+            IconOrImgUrl={Film}
+            title="Movies & TV"
+            url="/movies-tv"
+          />
+          <LargeSidebarItem IconOrImgUrl={Radio} title="Live" url="/live" />
+          <LargeSidebarItem
+            IconOrImgUrl={Gamepad2}
+            title="Gaming"
+            url="/gaming"
+          />
+          <LargeSidebarItem IconOrImgUrl={Newspaper} title="News" url="/news" />
+          <LargeSidebarItem
+            IconOrImgUrl={Trophy}
+            title="Sports"
+            url="/sports"
+          />
+          <LargeSidebarItem
+            IconOrImgUrl={Lightbulb}
+            title="Learning"
+            url="/learning"
+          />
+          <LargeSidebarItem
+            IconOrImgUrl={Shirt}
+            title="Fashion & Beauty"
+            url="/fashion-beauty"
+          />
+          <LargeSidebarItem
+            IconOrImgUrl={Podcast}
+            title="Podcasts"
+            url="/podcasts"
+          />
+        </LargeSidebarSection>
     </aside>
     </>
   )
 }
 
  type SmallSidebarItemProps = {
-    Icon:  ElementType
+    Icon:  ElementType 
     title: string
     url: string
  }
@@ -70,14 +130,15 @@ function LargeSidebarSection({ children, title, visibleItemCount = Number.POSITI
 }
 
 type LargeSidebarItemProps = {
-    Icon:  ElementType
+    IconOrImgUrl:  ElementType | string
     title: string
     url: string
-    isActive: boolean
+    isActive?: boolean
 }
-function LargeSidebarItem ({Icon, title, url, isActive = false, } : LargeSidebarItemProps) {
+function LargeSidebarItem ({IconOrImgUrl, title, url, isActive = false, } : LargeSidebarItemProps) {
     return (<a href={url} className={twMerge(buttonStyles({variant:"ghost"}), `w-full flex items-center rounded-lg gap-4 p-3 ${isActive? "font-bold bg-neutral-100 hover:bg-secondary" : undefined}`)} >
-        <Icon className="w-6 h-6" />
+        {typeof IconOrImgUrl === "string" ? (<img src={IconOrImgUrl}  className='w-6 h-6 rounded-full' />):(<IconOrImgUrl className="w-6 h-6" />)}
+        
         <div className="whitespace-nowrap overflow-hidden text-ellipsis">{title}</div>
 
     </a>) 
